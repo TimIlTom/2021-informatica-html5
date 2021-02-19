e3e00a4c6f780428e68b54ba8850041d85f797f1
Ionut Cicio <cicioionut328@gmail.com>
2021-02-19 21:27:44 +0100

    completed app based on wireframe

---
+++ see-the-magic/script.js
@@ -0,0 +1,18 @@
+const characters = {
+    'obi-wan-kenobi': 'HELLO THERE',
+    'cristiano-ronaldo': 'SIUUUM',
+    'voldemort': 'AVADAKEDAVRA',
+};
+
+function updateView() {
+    const keys = Object.keys(characters);
+    const character = keys[keys.length * Math.random() << 0];
+
+    $('#text').text(characters[character]);
+    $('#body').css(
+        'background-image',
+        `url('see-the-magic/assets/${character}.jpg')`
+    );
+}
+
+$(_ => $('#magic').on('click', updateView));
