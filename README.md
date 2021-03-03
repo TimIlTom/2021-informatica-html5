1ab076e409f19166e1b0f10cfc2e282a0cd3bd4a
repl.it user <replituser@example.com>
2021-03-03 15:53:43 +0000

    refactored code, added animations

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 2021-informatica-html
 
 Esercizio per informatica, IIS Marconi Civitavecchia
+
+Questo esercizio e' condiviso tra:
+- Toma Lupu (wireframe)
+- Ionut Cicio
+
+link repl: https://repl.it/@CuriousCI/2021-informatica-html5#README.md
--- magic/script.js
+++ magic/script.js
@@ -1,30 +1,32 @@
-const characters = {
-  'obi-wan-kenobi': 'HELLO THERE',
-  'cristiano-ronaldo': 'SIUUUM',
-  'voldemort': 'AVADAKEDAVRA',
-};
+random = {
+  current: '',
 
-let current;
-
-function pickCharacter() {
-  const keys = Object
-    .keys(characters)
-    .filter(character => character !== current); 
-
-  const character = keys[keys.length * Math.random() << 0];
-  current = character;
-
-  return character;
+  nextChoice(dictionary) {
+    const keys = Object
+      .keys(dictionary)
+      .filter(key => key != this.current);
+  
+    return this.current = keys[
+      Math.floor(keys.length * Math.random())
+    ];
+  }
 }
 
-function updateView() {
-  const character = pickCharacter();
+function draw() {
+  const characters = {
+    'obi-wan-kenobi': 'HELLO THERE',
+    'cristiano-ronaldo': 'SIUUUM',
+    'voldemort': 'AVADAKEDAVRA',
+  };
+
+  const character = random.nextChoice(characters);
 
   $('#text').text(characters[character]);
-  $('#body').css(
+  $('#text').addClass('padding');
+  $('body').css(
     'background-image',
     `url('magic/assets/${character}.jpg')`
   );
 }
 
-$(_ => $('#magic').on('click', updateView));
+$(_ => $('#magic').on('click', draw));
